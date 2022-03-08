5500a5a19bee97fe8238a9d6d8b736eaa28b6276
Your Name <you@example.com>
2022-03-07 21:24:03 -0300

    criando rotas

--- index.php
+++ index.php
@@ -5,9 +5,53 @@ require 'vendor/autoload.php';
 $app = new \Slim\app;
 
 
-$app->get('/postagem', function(){
+$app->get('/usuarios', function(){
 
-    echo "Ola mundo";
+    echo "Listagem de postagem ";
+
+});
+
+$app->get('/postagens[/{ano}[/{mes}]]', function($request, $response){ //com colchetes [] seria opcional 
+
+
+    $ano = $request->getAttribute('ano');
+    $mes = $request->getAttribute('mes');
+    // vberificar se ID é valido no Banco de dados
+
+    echo "Listagem de postagens Ano :  " . $ano . " mes: " . $mes;
+});
+
+    $app->get('/lista/{itens:.*}', function($request, $response){ 
+
+        $itens = $request->getAttribute('itens');
+        
+        // vberificar se ID é valido no Banco de dados
+    
+        //echo $itens;
+        var_dump(explode("/", $itens));
+
+});
+
+    $app->get('/blog/postagens/{id}: ' , function($request, $response){
+
+        echo 'lista de postagens';
+})->setName("blog");
+
+    $app->get('/meusite', function($request, $response){
+        $retorno = $this->get("router")->pathFor("blog", ["id" => "5"] );
+
+        echo $retorno;
+});
+
+$app->get('/v1/usuarios', function(){
+
+    echo "Listagem de usuarios ";
+
+});
+
+$app->get('/v1/usuarios', function(){
+
+    echo "Listagem de postagens ";
 
 });
 
