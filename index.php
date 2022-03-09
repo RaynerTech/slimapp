57c4e34cd87c3aff90b06d56633a9fa5f931b1f5
Your Name <you@example.com>
2022-03-09 17:09:26 -0300

    post

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
 
+use \Psr\Http\Message\ServeRequestInterface as rest;
+use \Psr\Http\Message\ResponseInterface as Response;
+
 require 'vendor/autoload.php';
 
 $app = new \Slim\app;
 
+$app->get('/postagens', function($request, Response $response){
 
+    $response->getBody()->write("Listagens de Postagens");
+    //echo "Lisntagens De Postagens";
+
+    return; $response;
+});
+
+
+
+/*
 $app->get('/usuarios', function(){
 
     echo "Listagem de postagem ";
@@ -43,16 +56,40 @@ $app->get('/postagens[/{ano}[/{mes}]]', function($request, $response){ //com col
         echo $retorno;
 });
 
-$app->get('/v1/usuarios', function(){
+//rotas
+$app->group('/v1/usuarios', function(){
 
     echo "Listagem de usuarios ";
 
+    $this->get('/usuarios', function(){
+
+        echo "Listagem de postagens ";
+    
+    
+    });
+
+    $this->get('/postagens', function(){
+
+        echo "Listagem de postagens ";
+    
+    
+    });
+
+
 });
 
-$app->get('/v1/usuarios', function(){
+*/
 
-    echo "Listagem de postagens ";
+//resquest Post
+$app->post('/usuarios/adiciona', function($request, Response $response){
 
+    //Recuperando post ($_Post)
+    $post = $request->getParsedBody();
+    $nome = $post['nome'];
+    $email = $post['email'];
+
+    return $response->getBody()->write($nome . "-" . $email);
+    
 });
 
 $app->run();
